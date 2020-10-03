8d6e00b48b8b5f1af6c3169f7b1daa5f9ccd66ef
nikolai akulov <nikolai.akulov@rtlabs.ru>
2020-10-03 09:02:29 +0300

    small fix task-3

--- lesson-4/3/main.go
+++ lesson-4/3/main.go
@@ -7,10 +7,12 @@ func main() {
 	var withdrawal int
 
 	fmt.Println("Банкомат")
+	fmt.Println("--------")
 	fmt.Println("Внимание: банкомат выдает купюры только номиналов 100 рублей и не больше 100000 рублей")
 	fmt.Println("Лимит на снятие не более 100000 рублей")
 	fmt.Println("Введите сумму для снятия")
 	fmt.Scan(&withdrawal)
+	fmt.Println("........")
 
 	if withdrawal > 100000 {
 		fmt.Println("Превышен допустимый лимит на снятие, укажите другую сумму")
@@ -19,12 +21,14 @@ func main() {
 	} else if withdrawal%100 != 0 {
 		fmt.Println("Указанная сумма не может быть выдана, укажите сумму кратную 100 рублей")
 	} else {
-		fmt.Println("Возьмите ваши деньги. Спасибо!")
-		for i := 1; i < withdrawal/100; i++ {
+		var note int
+		for note = 0; note < withdrawal/100; note++ {
 			fmt.Println("=========")
 			fmt.Println("=  100  =")
 			fmt.Println("=========")
 			fmt.Println("")
 		}
+		fmt.Printf("Выдано %d купюр номиналом 100 рублей\n", note)
+		fmt.Println("Возьмите ваши деньги. Спасибо!")
 	}
 }
