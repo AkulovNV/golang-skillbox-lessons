40aaaf499e771e5b04341330987ce12c113fff12
nikolai akulov <nikolai.akulov@rtlabs.ru>
2020-10-06 21:10:04 +0300

    change task7

--- lesson-5/7/main.go
+++ lesson-5/7/main.go
@@ -11,8 +11,9 @@ func main() {
 	fmt.Println("Счастливый билет")
 	fmt.Printf("Введите номер билета: ")
 	fmt.Scan(&ticket)
-
-	if ticket/100 == ticket%100 || ticket/100/10+(ticket/100)%10 == (ticket%100/10)+ticket%10 {
+	if ticket/1000 == ticket%10 && (ticket/100)%10 == (ticket%100)/10 {
+		fmt.Println("У вас зеркальный билет, круто! :)")
+	} else if ticket/100 == ticket%100 || ticket/1000+(ticket/100)%10 == (ticket%100/10)+ticket%10 {
 		fmt.Println("Поздравляем, у вас счастливый билет :)")
 	} else {
 		fmt.Println("Не повезло :(")
