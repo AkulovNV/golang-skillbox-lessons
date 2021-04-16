96f1c4b7c03ebc2f38c86fa9bfb28868a89746d8
nikolai <nik264555@gmail.com>
2021-04-16 15:52:53 +0300

    change task-4

--- lesson-14/4/main.go
+++ lesson-14/4/main.go
@@ -14,25 +14,22 @@ var (
 	num3 = 235
 )
 
-func sum1(a *int) int {
-	*a += num1 + num2
-	return *a
+func sum1(a int) (sum int) {
+	sum = a + num1 + num2
+	return
 }
-func sum2(a *int) int {
-	*a += num2 + num3
-	return *a
+func sum2(a int) (sum int) {
+	sum = a + num2 + num3
+	return
 }
-func sum3(a *int) int {
-	*a += num3 + num1
-	return *a
+func sum3(a int) (sum int) {
+	sum = a + num3 + num1
+	return
 }
 func main() {
 	var n int
 	fmt.Printf("Введите число: ")
 	fmt.Scan(&n)
-	sum1(&n)
-	sum2(&n)
-	sum3(&n)
 	fmt.Printf("Глобальные переменные: n1=%d, n2=%d, n3=%d\n", num1, num2, num3)
-	fmt.Println("Результат вычислений:", n)
+	fmt.Println("Результат вычислений:", sum1(sum2(sum3(n))))
 }
