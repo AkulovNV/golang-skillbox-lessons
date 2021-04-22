c7070b04ee982595b729deb5e929b9203189dcff
nikolai <nik264555@gmail.com>
2021-04-22 09:12:03 +0300

    change task-2

--- lesson-15/2/main.go
+++ lesson-15/2/main.go
@@ -16,9 +16,22 @@ func reversArr(arr [10]int) (revArr [10]int) {
 
 func main() {
 	var arr [10]int
-	for i := range arr {
-		fmt.Printf("Задайте элемент массива (число): ")
-		fmt.Scan(&arr[i])
+	var answer string
+	for {
+		fmt.Printf("Создать свой массив(y/n)? ")
+		fmt.Scan(&answer)
+		if answer == "y" {
+			for i := range arr {
+				fmt.Printf("Задайте элемент массива (число): ")
+				fmt.Scan(&arr[i])
+			}
+			break
+		} else if answer == "n" {
+			arr = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+			break
+		} else {
+			fmt.Println("Ошибочный ввод, нажмите 'y' или 'n'")
+		}
 	}
 	fmt.Println("Исходный массив:", arr)
 	fmt.Println("Реверсивный массив:", reversArr(arr))
